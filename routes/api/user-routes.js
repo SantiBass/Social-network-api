6fa314d71c4ca2c3b220fbe5bb9fc55badaea4e0
SantiBass <sibarraryan@gmail.com>
2022-02-04 12:18:10 -0800

    working on api/thought file

---
+++ routes/api/user-routes.js
@@ -0,0 +1,26 @@
+const router = require('express').Router();
+const {
+
+    getAllUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser
+} = require('../../controllers/user-controller');
+// api/users
+router
+    .route('/')
+    .get(getAllUsers)
+    .post(createUser);
+// api/users/:id
+router
+    .route('/:id')
+    .get(getUserById)
+    .put(updateUser)
+    .delte(deleteUser)
+
+    // route to add and remove friends
+router  
+    .route('/users/:userId/friends/:firendId')
+    .post(addNewFriend)
+    .delete(removeFriend);
