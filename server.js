4f10938a2a77b4a9f09ff6d3f2f0de6488b1545c
SantiBass <sibarraryan@gmail.com>
2022-02-04 10:49:38 -0800

    model/user

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT =process.env.PORT || 5002;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'));
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:SOCIAL-NETWOR-API',{
+ userNewUrlParser: true,
+ userUnifiedTopology: true   
+});
+mongoose.set('debug', true);
+app.use(require('./routes'));
+app.listen(PORT, ()=> console.log(`🌍 Connected on localhost:${PORT}`));
