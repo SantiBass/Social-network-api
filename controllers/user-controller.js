079f51a84a567e4ccd507e9a48aeb83bccc57d9e
SantiBass <sibarraryan@gmail.com>
2022-02-04 15:07:41 -0800

    working on controller/thought-controller file

---
+++ controllers/user-controller.js
@@ -0,0 +1,81 @@
+const { User } = require('../models');
+
+const userController = {
+  // get all users
+  getAllusers(req, res) {
+    User.find({})
+      .populate({
+        path: 'thoughts',
+        select: '-__v'
+      })
+      .select('-__v')
+      .sort({ _id: -1 })
+      .then(dbUserData => res.json(dbUserData))
+      .catch(err => {
+        console.log(err);
+        res.sendStatus(400);
+      });
+  },
+
+  // get one user by id
+  getUserById({ params }, res) {
+    Pizza.findOne({ _id: params.id })
+      .populate({
+        path: 'thoughts',
+        select: '-__v'
+      })
+      .select('-__v')
+      .then(dbUserData => res.json(dbUserData))
+      .catch(err => {
+        console.log(err);
+        res.sendStatus(400);
+      });
+  },
+
+  // createUser
+  createUser({ body }, res) {
+    User.create(body)
+      .then(dbUserData => res.json(dbUserData))
+      .catch(err => res.json(err));
+  },
+
+  // update pizza by id
+  updateUser({ params, body }, res) {
+    Pizza.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
+      .then(dbPizzaData => {
+        if (!dbPizzaData) {
+          res.status(404).json({ message: 'No user found with this id!' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => res.json(err));
+  },
+
+  // delete user
+  deleteUser({ params }, res) {
+    User.findOneAndDelete({ _id: params.id })
+      .then(dbUserData => res.json(dbUserData))
+      .catch(err => res.json(err));
+  },
+// delete friend
+removeFriend({ params}, res) {
+    User.findOneAndUpdate(
+        {
+            _id: params.getUserById
+        },
+        {
+            $pull: {
+                friends: params.friendId
+            }
+        },
+        {
+            new:true
+        }   
+    )
+}
+  
+
+};
+
+module.exports = pizzaController;
