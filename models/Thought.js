669d931248a4caa5da2da93609a7260895f381f1
SantiBass <sibarraryan@gmail.com>
2022-02-06 09:53:12 -0800

    working createAt error

--- models/Thought.js
+++ models/Thought.js
@@ -4,6 +4,36 @@ const moment = require('moment');
 // const { create } = require('./User');
 // const { format } = require('path/posix');
 // const { Thought } = require('.');
+
+const ReactionsSchema = new Schema({
+    reactionId:{
+        type: Schema.Types.ObjectId,
+        default: ()=> new Types.ObjectId()
+    },
+    reactionBody:{
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 280,
+    },
+    username:{
+        type: String,
+        required: true, 
+    },
+    createdAt:{
+        type: Date,
+        default: Date.now,
+        get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a') 
+    }
+
+},
+{ 
+    toJSON:{
+        getters: true  
+    }
+});
+
+
 const ThoughtSchema = new Schema({
     // add thoughtText, createdAt, usermane and reactions
     thoughtText:{
@@ -34,33 +64,7 @@ const ThoughtSchema = new Schema({
 
    
 );
-const ReactionsSchema = new Schema({
-    reactionId:{
-        type: Schema.Types.ObjectId,
-        default: ()=> new Types.ObjectId()
-    },
-    reactionBody:{
-        type: String,
-        required: true,
-        minlength: 1,
-        maxlength: 280,
-    },
-    username:{
-        type: String,
-        required: true, 
-    },
-    createdAt:{
-        type: Date,
-        default: Date.now,
-        get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a') 
-    }
 
-},
-{
-    toJSON:{
-        getters: true
-    }
-});
 //  getting total counst of thoughts
 ThoughtSchema.virtual('reactionCount').get(function() {
     return this.reaction.length;
